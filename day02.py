b6897a70b501132f6dc077c33a9466de28479db4
markusdlugi <dlugi.markus@gmail.com>
2020-12-02 06:12:25 +0100

    Add solution for day 2

---
+++ day02.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+lines = [line.strip() for line in open("input/02.txt")]
+
+valid_a = valid_b = 0
+for line in lines:
+    policy, password = line.split(": ")
+    ab, letter = policy.split(" ")
+    a, b = tuple(map(int, ab.split("-")))
+    count = Counter(password)[letter]
+    if a <= count <= b:
+        valid_a += 1
+    if bool(password[a - 1] == letter) ^ bool(password[b - 1] == letter):
+        valid_b += 1
+print(valid_a)
+print(valid_b)
