8c554dfbf3798c254e65b268777bd860aa955363
markusdlugi <dlugi.markus@gmail.com>
2020-12-03 06:48:54 +0100

    Rename x, y to avoid mixing them up with r, c

--- day03.py
+++ day03.py
@@ -1,13 +1,15 @@
 grid = [line.strip() for line in open("input/03.txt")]
 
+# print(*grid, sep="\n")
+
 slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
 results = []
-for x, y in slopes:
+for dc, dr in slopes:
     r, c = (0, 0)
     trees = 0
     while (r + 1) < len(grid):
-        c = (c + x) % len(grid[0])
-        r += y
+        c = (c + dc) % len(grid[0])
+        r += dr
         if grid[r][c] == "#":
             trees += 1
     results.append(trees)
