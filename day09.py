1578ae57bfc5ac734df155a735310da3a4cb0c04
markusdlugi <dlugi.markus@gmail.com>
2020-12-09 07:12:27 +0100

    Minor refactoring

--- day09.py
+++ day09.py
@@ -1,34 +1,34 @@
+from collections import deque
 from itertools import combinations
 
-lines = list(map(int, open("input/09.txt")))
+numbers = list(map(int, open("input/09.txt")))
 
 # Part A
 target = 0
-numbers = []
-for num in lines:
-    if len(numbers) < 25:
-        numbers.append(num)
+window = deque()
+for n in numbers:
+    if len(window) < 25:
+        window.append(n)
         continue
     valid = False
-    for a, b in combinations(numbers, 2):
-        if a + b == num:
+    for a, b in combinations(window, 2):
+        if a + b == n:
             valid = True
             break
     if not valid:
-        target = num
+        target = n
         break
-    numbers.append(num)
-    del numbers[0]
+    window.append(n)
+    window.popleft()
 print(target)
 
 # Part B
-numbers = []
-for num in lines:
-    numbers.append(num)
-    sum_ = sum(numbers)
+window = deque()
+for n in numbers:
+    window.append(n)
+    sum_ = sum(window)
     while sum_ > target:
-        sum_ -= numbers[0]
-        del numbers[0]
+        sum_ -= window.popleft()
     if sum_ == target:
-        print(min(numbers) + max(numbers))
+        print(min(window) + max(window))
         break
