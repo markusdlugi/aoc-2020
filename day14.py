c1580cb68dafa05dbb231c26f09cc44576a57e92
markusdlugi <dlugi.markus@gmail.com>
2020-12-14 12:35:43 +0100

    Minor refactoring

--- day14.py
+++ day14.py
@@ -12,26 +12,26 @@ for line in lines:
         address, number = line.split(" = ")
         address = int(address[4:-1])
         number = int(number)
-        value_byte = f"{number:036b}"
-        address_byte = f"{address:036b}"
+        value_bin = f"{number:036b}"
+        address_bin = f"{address:036b}"
 
         floating = []
-        if bitmask is not None:
-            for i, v in enumerate(bitmask):
-                # Change value in v1 if 0 or 1
-                if v != "X":
-                    value_byte = value_byte[:i] + v + value_byte[i + 1:]
+        assert bitmask is not None
+        for i, v in enumerate(bitmask):
+            # Change value in v1 if 0 or 1
+            if v != "X":
+                value_bin = value_bin[:i] + v + value_bin[i + 1:]
 
-                # Change address in v2 if not 0
-                if v == "1":
-                    address_byte = address_byte[:i] + v + address_byte[i+1:]
-                elif v == "X":
-                    floating.append(i)
-        mem_v1[address] = int(value_byte, 2)
+            # Change address in v2 if 1 or X
+            if v == "1":
+                address_bin = address_bin[:i] + v + address_bin[i + 1:]
+            elif v == "X":
+                floating.append(i)
+        mem_v1[address] = int(value_bin, 2)
 
-        for comb in product(["0", "1"], repeat=len(floating)):
-            address = address_byte
-            for i, v in enumerate(comb):
+        for floating_bits in product(["0", "1"], repeat=len(floating)):
+            address = address_bin
+            for i, v in enumerate(floating_bits):
                 bit = floating[i]
                 address = address[:bit] + v + address[bit+1:]
             mem_v2[int(address, 2)] = number
