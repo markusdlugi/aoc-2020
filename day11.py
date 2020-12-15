ba30eed1d2a8ac7d6b0e0638adb9bea7c85db1c6
Markus Dlugi <dlugi.markus@gmail.com>
2020-12-16 00:16:21 +0100

    Optimize day 11

--- day11.py
+++ day11.py
@@ -1,71 +1,74 @@
-from collections import Counter
+from collections import defaultdict
+from copy import deepcopy
 from timeit import default_timer as timer
 
-visible_seats = dict()
+dr, dc = ((-1, -1, 0, 1, 1, 1, 0, -1), (0, 1, 1, 1, 0, -1, -1, -1))
+visible_seats = defaultdict(list)
 
 
-def moves(grid, x, y, new_rules):
-    global visible_seats
-    dx, dy = ((0, 1, 1, 1, 0, -1, -1, -1), (-1, -1, 0, 1, 1, 1, 0, -1))
+def in_grid(r, c, R, C):
+    return 0 <= r < R and 0 <= c < C
+
+
+def count_occupied(G, r, c, R, C, new_rules):
+    global dr, dc, visible_seats
+    sum_ = 0
+    if visible_seats[(r, c)]:
+        return sum(1 for (rr, cc) in visible_seats[(r, c)] if G[rr][cc] == "#")
     for i in range(8):
-        if (x, y, i) in visible_seats:
-            new_pos = visible_seats[(x, y, i)]
-        else:
-            new_pos = (x + dx[i], y + dy[i])
-            while new_rules and new_pos in grid and grid[new_pos] == ".":
-                new_pos = (new_pos[0] + dx[i], new_pos[1] + dy[i])
-            visible_seats[(x, y, i)] = new_pos
-        yield grid[new_pos] if new_pos in grid else None
+        rr, cc = (r + dr[i], c + dc[i])
+        while new_rules and in_grid(rr, cc, R, C) and G[rr][cc] == ".":
+            rr, cc = (rr + dr[i], cc + dc[i])
+        if in_grid(rr, cc, R, C):
+            visible_seats[(r, c)].append((rr, cc))
+            if G[rr][cc] == "#":
+                sum_ += 1
+    return sum_
 
 
-def show(grid, max_x, max_y):
-    for y in range(0, max_y + 1):
-        for x in range(0, max_x + 1):
-            print(grid[(x, y)], sep='', end='')
-        print()
+def show(G, R):
+    for r in range(0, R):
+        print("".join(G[r]))
     print()
 
 
-def simulate(grid, new_rules):
+def simulate(G, R, C, new_rules):
     changes = True
     while changes:
         changes = False
-        new_grid = grid.copy()
+        new_G = deepcopy(G)
 
-        for (x, y), state in grid.items():
-            adj_count = Counter(moves(grid, x, y, new_rules))["#"]
-            if state == "L" and adj_count == 0:
-                new_grid[(x, y)] = "#"
-                changes = True
-            if state == "#" and adj_count >= (5 if new_rules else 4):
-                new_grid[(x, y)] = "L"
-                changes = True
-        grid = new_grid
-        # show(grid, max_x, max_y)
-    return grid
+        for r in range(R):
+            for c in range(C):
+                state = G[r][c]
+                if state == ".":
+                    continue
+                adj_count = count_occupied(G, r, c, R, C, new_rules)
+                if state == "L" and adj_count == 0:
+                    new_G[r][c] = "#"
+                    changes = True
+                if state == "#" and adj_count >= (5 if new_rules else 4):
+                    new_G[r][c] = "L"
+                    changes = True
+        G = new_G
+        # show(G, R)
+    return G
 
 
-lines = [line.strip() for line in open("input/11.txt")]
-
-grid = dict()
-for y, line in enumerate(lines):
-    for x, c in enumerate(line):
-        grid[(x, y)] = c
-
-max_x, max_y = (x, y)
+G = [list(line.strip()) for line in open("input/11.txt")]
+R, C = (len(G), len(G[0]))
 
 # Part A
 start = timer()
-original_grid = grid.copy()
-grid = simulate(grid, False)
-occ = Counter(grid.values())["#"]
-print(occ)
+OG = deepcopy(G)
+G = simulate(G, R, C, False)
+print(sum(r.count("#") for r in G))
 
 # Part B
 visible_seats.clear()
-grid = original_grid.copy()
-grid = simulate(grid, True)
-occ = Counter(grid.values())["#"]
-print(occ)
+G = deepcopy(OG)
+G = simulate(G, R, C, True)
+print(sum(r.count("#") for r in G))
+
 end = timer()
 print(f'Took {end - start} seconds.')
